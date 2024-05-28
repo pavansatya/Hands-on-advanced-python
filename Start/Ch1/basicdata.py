1c6a1e06ac9a3534c7373c576748c2e53a317b3a
pavansatya <krishvenigalla06@gmail.com>
2024-05-28 10:20:18 -0400

    commit

--- Start/Ch1/basicdata.py
+++ Start/Ch1/basicdata.py
@@ -9,10 +9,18 @@ with open("../../sample-weather-history.json", "r") as weatherfile:
     weatherdata = json.load(weatherfile)
 
 # TODO: What was the warmest day in the data set?
-
+warmest_day = max(weatherdata, key=lambda x:x['tmax'])
+print("The warmest day is {} with {} degrees".format(warmest_day['date'], warmest_day['tmax']))
 
 # TODO: What was the coldest day in the data set?
+coldest_day = min(weatherdata, key=lambda x:x['tmin'])
+print("The coldest day is {} with {} degrees".format(coldest_day['date'], coldest_day['tmin']))
 
 
 # TODO: How many days had snowfall?
+count = 0
+for day in weatherdata:
+    if day['snow'] > 0.0:
+        count = count + 1
 
+print(count)        
